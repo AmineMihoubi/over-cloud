18056d01d4b05970b398d5463ebf7d13f2a20574
AmineMihoubi <64211553+AmineMihoubi@users.noreply.github.com>
2021-03-01 11:55:48 -0500

    Ajout d'une page de connexion

---
+++ index.php
@@ -0,0 +1,31 @@
+<html>
+    <head>
+       <meta charset="utf-8">
+        <!-- importer le fichier de style -->
+        <link rel="stylesheet" href="/Over-Cloud/css/connexion.css" media="screen" type="text/css" />
+    </head>
+    <body>
+        <div id="container">
+            <!-- zone de connexion -->
+            
+            <form action="verification.php" method="POST">
+                <h1>Connexion</h1>
+                
+                <label><b>Adresse e-mail</b></label>
+                <input type="text" placeholder="Entrer votre adresse mail" name="email" required>
+
+                <label><b>Mot de passe</b></label>
+                <input type="password" placeholder="Entrer le mot de passe" name="password" required>
+
+                <input type="submit" id='submit' value='LOGIN' >
+                <?php
+                if(isset($_GET['erreur'])){
+                    $err = $_GET['erreur'];
+                    if($err==1 || $err==2)
+                        echo "<p style='color:red'>Email ou mot de passe incorrect</p>";
+                }
+                ?>
+            </form>
+        </div>
+    </body>
+</html>
---
+++ verification.php
@@ -0,0 +1,52 @@
+<?php
+session_start();
+if(isset($_POST['email']) && isset($_POST['password']))
+{
+    // connexion à la base de données
+    $db_username = 'root';
+    $db_password = '';
+    $db_name     = 'overcloud';
+    $db_host     = 'localhost';
+    $db = mysqli_connect($db_host, $db_username, $db_password,$db_name)
+           or die('could not connect to database');
+    
+    // on applique les deux fonctions mysqli_real_escape_string et htmlspecialchars
+    // pour éliminer toute attaque de type injection SQL et XSS
+    $email = mysqli_real_escape_string($db,htmlspecialchars($_POST['email'])); 
+    $password = mysqli_real_escape_string($db,htmlspecialchars($_POST['password']));
+    
+    if($email !== "" && $password !== "")
+    {
+        $requete = "SELECT count(*) FROM utilisateur where 
+              email = '".$email."' and password = '".$password."' ";
+        $exec_requete = mysqli_query($db,$requete);
+        $reponse      = mysqli_fetch_array($exec_requete);
+        $count = $reponse['count(*)'];
+        if($count!=0) // nom d'utilisateur et mot de passe correctes
+        {
+           $_SESSION['email'] = $email;
+
+           $requete = "SELECT id_utilisateur, nom FROM utilisateur WHERE email = '".$email."'";
+           $exec_requete = mysqli_query($db,$requete);
+           $reponse      = mysqli_fetch_assoc($exec_requete);
+           $_SESSION['idUtilisateur'] = $reponse['id_utilisateur'];
+           $_SESSION['nom'] = $reponse['nom'];
+
+           header('Location: parametresGalerie.html');
+        }
+        else
+        {
+           header('Location: index.php?erreur=1'); // utilisateur ou mot de passe incorrect
+        }
+    }
+    else
+    {
+       header('Location: index.php?erreur=2'); // utilisateur ou mot de passe vide
+    }
+}
+else
+{
+   header('Location: index.php');
+}
+mysqli_close($db); // fermer la connexion
+?>
