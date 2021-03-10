e8561a0ebf2d69dbfbb18f9b4c45d405acf1a6cb
Uniik <50687831+Uniik@users.noreply.github.com>
2021-03-10 18:46:05 -0500

    insert du nouvel utilisateur
    HELP! le code en php devrait ètre appelé après avoir cliqué sur le boutton, pas avant...
    J'ai utilisé ca: https://www.geeksforgeeks.org/how-to-call-php-function-on-the-click-of-a-button/

--- inscription.php
+++ inscription.php
@@ -11,13 +11,46 @@
 <div class="wrapper">
 	<div class="container">
 		<h1>Bienvenue</h1>
+
+		<?php
+
+			if(isset($_POST['sub-btn'])) {
+				$db_username = 'root';
+				$db_password = '';
+				$db_name     = 'overcloud';
+				$db_host     = 'localhost';
+
+				//Connecting to the database
+				$conn = mysqli_connect($db_host, $db_username, $db_password,$db_name);
+
+				if(!$conn){
+					die("could not connect to database: " . mysqli_connect_error());
+				}
+
+				$email = mysqli_real_escape_string($conn,htmlspecialchars($_POST['mail']));
+				$username = mysqli_real_escape_string($conn,htmlspecialchars($_POST['username']));
+    			$password = mysqli_real_escape_string($conn,htmlspecialchars($_POST['pwd']));
+				$password2 = mysqli_real_escape_string($conn,htmlspecialchars($_POST['pwd2']));
+				if($password == $password2){
+					$sql="insert into utilisateur (nom, password, Email) VALUES ($username, $password, $email)";
+					$res=mysqli_query($conn,$sql);
+					if($res){
+						echo "test";
+					}else{
+						echo "patate";
+					}
+				}
+			} 
+
+		?>
 		
-		<form class="form">
-			<input type="text" placeholder="E-mail">
-			<input type="text" placeholder="Nom d'utilisateur">
-			<input type="password" placeholder="Mot de passe">
-			<input type="password" placeholder="Confirmation">
-			<button type="submit" id="sub-button">Inscription</button>
+		<form method="post">
+			<input type="text" name="mail" placeholder="E-mail">
+			<input type="text" name="username" placeholder="Nom d'utilisateur">
+			<input type="password" name="pwd" placeholder="Mot de passe">
+			<input type="password" name="pwd2" placeholder="Confirmation">
+			<input type="submit" id="sub-button" name="sub-btntsdag" value="Inscription" />
+			
 			<button id="goto-login">Déjà inscrit?</button>
 		</form>
 	</div>
