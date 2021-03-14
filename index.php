35f97055fa4b7c11339531321714c7026ea47386
nimjawarior <59122599+nimjawarior@users.noreply.github.com>
2021-03-14 16:41:07 -0400

    Correction de la page de login pour garder les effets visuel tout en étant fonctionnel

--- index.php
+++ index.php
@@ -1,40 +1,58 @@
 <!importé de Lewi Hussey>
-<html lang="en" >
-<head>
-  <meta charset="UTF-8">
-  <title>CodePen - Calm breeze login screen</title>
-  <link rel="stylesheet" href="./css/login.css">
+	<html lang="en">
 
-</head>
-<body>
-<!-- partial:index.partial.html -->
-<div class="wrapper">
-	<div class="container">
-		<h1>Bienvenue</h1>
-		
-		<form class="form">
-			<input type="text" placeholder="E-mail">
-			<input type="password" placeholder="Mot de passe">
-			<button type="submit" id="login-button">Connexion</button>
-			<button id="goto-subscribe">Pas de compte?</button>
-		</form>
-	</div>
-	
-	<ul class="bg-bubbles">
-		<li></li>
-		<li></li>
-		<li></li>
-		<li></li>
-		<li></li>
-		<li></li>
-		<li></li>
-		<li></li>
-		<li></li>
-		<li></li>
-	</ul>
-</div>
-<!-- partial -->
-  <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script><script  src="./js/login.js"></script>
+	<head>
+		<meta charset="UTF-8">
+		<title>CodePen - Calm breeze login screen</title>
+		<link rel="stylesheet" href="./css/login.css">
 
-</body>
-</html>
+	</head>
+
+	<body>
+		<!-- partial:index.partial.html -->
+		<div class="wrapper">
+			<div class="container">
+
+				<form class="form" action="verification.php" method="POST">
+					<h1>Connexion</h1>
+
+					<label>Adresse e-mail</label>
+					<input type="text" placeholder="Entrer votre adresse mail" name="email" required>
+
+					<label>Mot de passe</label>
+					<input type="password" placeholder="Entrer le mot de passe" name="password" required>
+
+					<input type="submit" id='login-button' value='LOGIN'>
+					<?php
+					if (isset($_GET['erreur'])) {
+						$err = $_GET['erreur'];
+						if ($err == 1 || $err == 2)
+							echo "<p style='color:red'>Email ou mot de passe incorrect</p>";
+					}
+					?><h4 id=goto-subscribe onclick="myFunction()">Créer un compte !</h4>
+				</form>
+			</div>
+
+			<ul class="bg-bubbles">
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+			</ul>
+		</div>
+		<!-- partial -->
+		<script>
+			function myFunction() {
+				window.location.href = "inscription.php";
+			}
+		</script>
+
+	</body>
+
+	</html>
