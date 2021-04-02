c741fd9ccc3f0ecd7798b3a8d9d7b2ad8c7a22e1
AmineMihoubi <64211553+AmineMihoubi@users.noreply.github.com>
2021-04-01 20:56:41 -0400

    Début des pas pour les administrateurs du site

---
+++ admin/navigationBar-admin.php
@@ -0,0 +1,11 @@
+<!-- Bare de navigation !-->
+
+<ul>
+    <h2>
+        <a>Admin</a>
+    </h2>
+    <li><a href="listeUtilisateur.php">Liste des utilisateurs</a></li>
+    <li><a href="parametresGalerie.php">Liste des galeries</a></li>
+    <li><a href="">Liste des albums</a></li>
+    <li id="deconnexion"><a href="../php/deconnexion.php">Se déconnecter</a></li>
+</ul>
---
+++ admin/panel-admin.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <!-- importer le fichier de style -->
+    <link rel="stylesheet" href="../css/admin.css" media="screen" type="text/css" />
+    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+</head>
+
+
+<body>
+
+          <!-- Bare de navigation !-->
+          <div id=navigationBar></div>
+    <script>
+        $(function() {
+            $("#navigationBar").load("navigationbar-admin.php");
+        });
+    </script>
+
+</body>
