ee4d42d3f279c0bf72fae2e89a8ab1982d9e7130
nahwa <bbidori@users.noreply.github.com>
2021-05-10 18:28:46 -0400

    changer le ui du modal pour ajouter une photos

--- pages/photoAlbum.php
+++ pages/photoAlbum.php
@@ -19,18 +19,13 @@ $row1 =  mysqli_fetch_array($resultType);
 $typeAlbum = $row1['prive'];
 $nomGalerie = $row1['nom'];
 
-date_default_timezone_set('America/New_york');
-$currentDate = date("Y-m-d H:i:s");
 
-//echo "<script>alert('Prive : $typeAlbum ');</script>";
+
 if ($typeAlbum == 1) {
     $ownerSQL = "SELECT fk_id_utilisateur FROM utilisateur_galerie where fk_id_galerie = '$id'";
     $resultOwner = mysqli_query($db, $ownerSQL);
     $row2 =  mysqli_fetch_array($resultOwner);
     $ownerID = $row2['fk_id_utilisateur'];
-    //echo "<script>alert('Owner id: $ownerID ');</script>";
-    //$loginID = $_SESSION['idUtilisateur'];
-    //echo "<script>alert('Logged in ID: $loginID');</script>";
     if ($_SESSION['idUtilisateur'] != $ownerID) {
         header('Location: 404.php');
     }
@@ -38,47 +33,6 @@ if ($typeAlbum == 1) {
 $_SESSION['urlPrecedent'] = $_SERVER['REQUEST_URI'];
 ?>
 
-<?php
-if (isset($_POST['upload'])) {
-    $image = addslashes(file_get_contents($_FILES['image']['tmp_name']));
-    $sql = "INSERT INTO photo(photo,date,fk_id_galerie) VALUES ('$image',curdate(), '$idGalerie')";
-    $sql2 = "INSERT INTO photo_album(fk_id_photo,fk_id_album) VALUES (LAST_INSERT_ID(),'$idAlbum')";
-    //IL FAUT VRAIMENT LES EXCUTES EN ORDRE A CAUSE DU LAST_INSERT_ID()
-    $sql3 = "INSERT INTO historique(fk_id_utilisateur, action, date) VALUES ('{$_SESSION['idUtilisateur']}', 'à ajouté une photo dans $nomAlbum($nomGalerie)', curdate())";
-
-    // Execute query
-    if (mysqli_query($db, $sql)) {
-        if (mysqli_query($db, $sql2)) {
-            $page = $_SESSION['urlPrecedent'];
-            echo "<br/>YAY.";
-            echo "<script> window.location.replace('$page'); </script>";
-        }
-        else {
-            echo "<br/>NOOO2.";
-        }
-    } else {
-        echo "<br/>NOOO.";
-    }
-    header("Refresh: 0.001; photoAlbum.php?id=$idAlbum");
-
-}
-
-if (isset($_POST['confirm'])) {
-    $sql2 = "DELETE FROM photo where id_album like $idAlbum";
-    if (mysqli_query($db, $sql2)) {
-        $sql = "DELETE FROM album where id_album like $idAlbum";
-        // Execute query
-        if (mysqli_query($db, $sql)) {
-            $page = $_SESSION['urlPrecedent'];
-            echo "<br/>YAY.";
-            echo "<script> document.getElementsByClassName('.bg-popup2').style.display = 'none'; </script>";
-            echo "<script> window.location.replace('$page'); </script>"; //replace la page courante a la page voulu, dans ce cas, la page precedente
-        } else {
-            echo "<br/>NOOO.";
-        }
-    }
-}
-?>
 
 <!DOCTYPE html>
 <html>
@@ -93,63 +47,29 @@ if (isset($_POST['confirm'])) {
 
 <!--afficher photos dans l'album-->
 <?php
-$idAlbum = $_GET['id'];
 $sql_afficher_photos = "SELECT * FROM photo p join photo_album pa on p.id_photo = pa.fk_id_photo where pa.fk_id_album = $idAlbum";
 $res = mysqli_query($db, $sql_afficher_photos);
 ?>
 
 <body>
+
+    <!--Barre de navigation-->
     <div id=navigationBar></div>
-    <!--navigation-->
     <script>
         $(function() {
             $("#navigationBar").load("navigationbar.php");
         });
     </script>
 
+    <!--Barre d'actions-->
     <div style="margin-left: 200px;">
-        <!--content-->
         <div class="actionsbar-container">
             <ul>
-                <li><a id="btn-ajouter" class="button">Ajouter +</a></li>
-                <li> <a id="button" onClick="supprimerAlbum()"> Supprimer album</a> </li>
+                <li><a id="btn-ajouter" class="button" onclick="document.getElementById('ajouter-photos').style.display='block'">Ajouter +</a></li>
+                <li> <a id="btn-supprimer" class="button"> Supprimer album</a> </li>
             </ul>
-
-        </div>
-
-
-        <script>
-            function supprimerAlbum() {
-                document.querySelector('.bg-popup2').style.display = 'flex';
-            }
-        </script>
-
-    </div>
-
-
-    <div class="bg-popup">
-        <div class="popup-content">
-            <div class="btn-fermer">+</div>
-            <form method="POST" action="" enctype="multipart/form-data">
-                <br /><br /><br /><br /><br /><br />
-                <input class="popup-input" type="file" name="image">
-                <input class="popup-input" type="submit" name="upload" value="Upload"></li>
-            </form>
-
         </div>
     </div>
-
-    <div class="bg-popup2">
-        <div class="popup-content">
-            <div class="btn-fermer">+</div>
-            <form method="POST" action="" enctype="multipart/form-data">
-                <br /><br /><br /><br /><br /><br />
-                <input class="popup-input" type="submit" name="confirm" value="Confirmer"></li>
-            </form>
-
-        </div>
-    </div>
-
 
     <div class="gallery-container">
         <div class="gallery">
@@ -172,9 +92,87 @@ $res = mysqli_query($db, $sql_afficher_photos);
             ?>
         </div>
     </div>
+
+
+    <div id="ajouter-photos" class="popup">
+        <span onclick="document.getElementById('ajouter-photos').style.display='none'" class="close" title="Close Modal">&times;</span>
+        <form class="popup-content" method="POST" action="" enctype="multipart/form-data">
+            <div class="popup-container">
+                <h1>AJOUTER PHOTOS</h1>
+                <div class="popup-buttons">
+                    <input class="popup-input" type="file" name="image">
+                    <input class="popup-input" type="submit" name="upload" value="Upload"></li>
+                </div>
+            </div>
+        </form>
+
     </div>
 
-    <script src="../js/popupscript.js"></script>
+    <script>
+        var modal = document.getElementById('ajouter-photos');
+        window.onclick = function(event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+        }
+    </script>
+
+
+    <!-- <div class="supprimer-photos">
+
+        <form method="POST" action="" enctype="multipart/form-data">
+            <br /><br /><br /><br /><br /><br />
+            <input class="popup-input" type="submit" name="confirm" value="Confirmer"></li>
+        </form>
+
+    </div> -->
+
+
+    <?php
+    if (isset($_POST['upload'])) {
+        $image = addslashes(file_get_contents($_FILES['image']['tmp_name']));
+        $sql = "INSERT INTO photo(photo,date,fk_id_galerie) VALUES ('$image',curdate(), '$idGalerie')";
+        $sql2 = "INSERT INTO photo_album(fk_id_photo,fk_id_album) VALUES (LAST_INSERT_ID(),'$idAlbum')";
+        //IL FAUT VRAIMENT LES EXCUTES EN ORDRE A CAUSE DU LAST_INSERT_ID()
+        $sql3 = "INSERT INTO historique(fk_id_utilisateur, action, date) VALUES ('{$_SESSION['idUtilisateur']}', 'à ajouté une photo dans $nomAlbum($nomGalerie)', curdate())";
+
+        // Execute query
+        if (mysqli_query($db, $sql)) {
+            if (mysqli_query($db, $sql2)) {
+                $page = $_SESSION['urlPrecedent'];
+                echo "<br/>YAY.";
+                echo "<script> window.location.replace('$page'); </script>";
+            } else {
+                echo "<br/>NOOO2.";
+            }
+        } else {
+            echo "<br/>NOOO.";
+        }
+        header("Refresh: 0.001; photoAlbum.php?id=$idAlbum");
+    }
+
+    if (isset($_POST['confirm'])) {
+        $sql2 = "DELETE FROM photo where id_album like $idAlbum";
+        if (mysqli_query($db, $sql2)) {
+            $sql = "DELETE FROM album where id_album like $idAlbum";
+            // Execute query
+            if (mysqli_query($db, $sql)) {
+                $page = $_SESSION['urlPrecedent'];
+                echo "<br/>YAY.";
+                echo "<script> document.getElementsByClassName('.bg-popup2').style.display = 'none'; </script>";
+                echo "<script> window.location.replace('$page'); </script>"; //replace la page courante a la page voulu, dans ce cas, la page precedente
+            } else {
+                echo "<br/>NOOO.";
+            }
+        }
+    }
+    ?>
+
+
+    
+    
+
+
 </body>
 
 </html>
