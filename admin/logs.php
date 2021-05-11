eabf2ed3bb532e8b6169d4c8107496818d7243fe
AmineMihoubi <64211553+AmineMihoubi@users.noreply.github.com>
2021-05-11 12:56:37 -0400

    Ajoute du tableau des logs pour les admins

--- admin/logs.php
+++ admin/logs.php
@@ -1,9 +1,9 @@
 <?php
-    session_start();
-    require '../php/ConnectDb.php';
-    $db = ConnectDb::getInstance();
-    $sql = "SELECT u.courriel, h.action, h.date FROM historique h join utilisateur u on u.id_utilisateur = h.fk_id_utilisateur ORDER BY h.date";
-    $result = mysqli_query($db, $sql);
+session_start();
+require '../php/ConnectDb.php';
+$db = ConnectDb::getInstance();
+$sql = "SELECT u.courriel, h.action, h.date FROM historique h join utilisateur u on u.id_utilisateur = h.fk_id_utilisateur ORDER BY h.date";
+$result = mysqli_query($db, $sql);
 ?>
 <!DOCTYPE html>
 <html>
@@ -19,28 +19,56 @@
 <body>
     <div>
         <!-- Bare de navigation !-->
-    <div id=navigationBar></div>
-    <script>
-        $(function() {
-            $("#navigationBar").load("navigationbar-admin.php");
-        });
-    </script>
-        <div style="margin-left: 200px;">
-            <div>
-                <?php
-                    if (mysqli_num_rows($result) > 0) {
-                        while ($row = mysqli_fetch_assoc($result)) {
-                            $utilisateur = $row['courriel']; 
-                            $action = $row['action'];
-                            $date = $row['date'];
-                            echo "$utilisateur   $action  -  $date <br>";
-                        }
-                    } else {
-                        echo "0 results";
-                    }
-                ?>
+        <div id=navigationBar></div>
+        <script>
+            $(function() {
+                $("#navigationBar").load("navigationbar-admin.php");
+            });
+        </script>
+
+        <div class='carte'>
+            <div id=infoText>
+                <i>La liste des logs contient tout les changements faites par les admins</i>
+            </div>
+
+            <div class='listeUtilisateurs'>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Utilisateur</th>
+                            <th>Description</th>
+                            <th>Date</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                          $sql = "SELECT id_administrateur,description,date FROM administrateur_changement";
+                          $result = mysqli_query($db, $sql);
+                          while ($row =  mysqli_fetch_array($result)) {
+                          $idAdmin = $row['id_administrateur'];    
+                          $description = $row['description'];
+                          $date = $row['date'];  
+                          $requete = "SELECT utilisateur FROM administrateur WHERE id_admin = $idAdmin";
+                          $exec_requete = mysqli_query($db,$requete);
+                          $reponse      = mysqli_fetch_array($exec_requete);
+                          $utilisateur = $reponse['utilisateur'];
+                          echo 
+                          "
+                          <tr>
+                          <td>$utilisateur</td>
+                          <td>$description</td>
+                          <td>$date</td>
+                          </tr>
+                          ";     
+                          }  
+                        ?>
+                    </tbody>
+                </table>
             </div>
         </div>
+
+
     </div>
 </body>
+
 </html>
