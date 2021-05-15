d29e14c5f551d9b35f873b331274b1228d8b3b4b
nahwa <bbidori@users.noreply.github.com>
2021-05-14 21:49:05 -0400

    ajout des commentaires mais il faut le rendre plus beau je trouve

--- php/gererCommentaire.php
+++ php/gererCommentaire.php
@@ -12,25 +12,23 @@ $idPhoto = $_POST['idPhoto'];
 $sql = "DELETE FROM commentaire WHERE id_commentaire = $idCommentaire";
 mysqli_query($db,$sql);
 mysqli_close($db);
-header("Refresh: 0.001; ../pages/photo-zoom?idPhoto=$idPhoto");
-echo "<script>alert('Votre commentaire a été supprimé !');</script>";
-
-}
-
-
-if ($_POST['submit-envoyer']) {
-$text = mysqli_real_escape_string($db,htmlspecialchars($_POST['commentaire']));   
-$idPhoto = $_POST['idPhoto'];
-$idUtilisateur = $_SESSION['idUtilisateur'];
-date_default_timezone_set('America/New_york');
-$date = date("Y-m-d H:i:s");
-
-$sql = "INSERT INTO commentaire (fk_id_auteur,fk_id_photo,message,date) VALUES ('".$idUtilisateur."', $idPhoto, '".$text."','".$date."' )";
-mysqli_query($db,$sql);
-mysqli_close($db);
-header("Refresh: 0.001; ../pages/photo-zoom?idPhoto=$idPhoto");
+header("Refresh: 0.001; ../pages/photos.php");
 
 
 }
+
+
+if ($_POST['ajouter-commentaire']) {
+    $text = mysqli_real_escape_string($db,htmlspecialchars($_POST['ajouter-commentaire']));   
+    $idPhoto = $_POST['idPhoto'];
+    $idUtilisateur = $_SESSION['idUtilisateur'];
+    
+    $sql = "INSERT INTO commentaire (fk_id_auteur,fk_id_photo,message) VALUES ('".$idUtilisateur."', $idPhoto, '".$text."' )";
+    mysqli_query($db,$sql);
+    mysqli_close($db);
+    
+    header("Refresh: 0.001; ../pages/photos.php");
+    
+    }
 
 ?>
